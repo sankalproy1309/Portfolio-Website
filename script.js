de00660d186ad72d3c9bf169dde374f7b9a3b500
Sankalp <sankalproy2004@gmail.com>
2025-03-27 12:15:56 +0530

    Fix: Typed.js typing effect not working issue

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var typed = new Typed('#element', {
-    strings: ['Web Developer', 'Photographer', 'Videographer', 'Video Editor', 'Traveller'],
-    typeSpeed: 70,
-    loop: true,
-    backSpeed: 50,
-    backDelay: 100,
-});
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('#element', {
+      strings: ['Web Developer', 'Photographer', 'Videographer', 'Video Editor', 'Traveller'],
+      typeSpeed: 70,
+      backSpeed: 50,
+      backDelay: 100,
+      loop: true
+    });
+  });
+  
