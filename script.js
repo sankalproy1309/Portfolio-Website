3cc11b8d95acfe091bd20693c722236ffb9b54da
Sankalp <sankalproy2004@gmail.com>
2025-03-27 10:59:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Photographer', 'Videographer', 'Video Editor', 'Traveller'],
+    typeSpeed: 70,
+    loop: true,
+    backSpeed: 50,
+    backDelay: 100,
+});
