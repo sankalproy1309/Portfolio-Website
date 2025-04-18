eb1bd74dafdd5059fc2f0d45371d98bda8fd34a4
Sankalp <sankalproy2004@gmail.com>
2025-04-18 16:06:08 +0530

    Your commit message here

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var typed = new Typed('#element', {
-      strings: ['Web Developer', 'Photographer', 'Videographer', 'Video Editor', 'Traveller'],
-      typeSpeed: 70,
-      backSpeed: 50,
-      backDelay: 100,
-      loop: true
-    });
+  var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Photographer', 'Videographer', 'Video Editor', 'Traveller'],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
   });
-  
+});
